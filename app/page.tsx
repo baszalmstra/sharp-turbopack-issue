483e54660b7155678f064a5c91509d1cb890d6c8
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-02-15 11:22:20 +0100

    plaiceholder usecase

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
-import sharp from 'sharp';
+import { getPlaiceholder } from "plaiceholder";
+import Image from "next/image";
 
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  sharp();
+export default async function Home() {
+  const src =
+    "https://images.unsplash.com/photo-1621961458348-f013d219b50c";
+  const buffer = await fetch(src).then(async (res) =>
+    Buffer.from(await res.arrayBuffer())
+  );
 
-  return null
+  const { base64 } = await getPlaiceholder(buffer);
+
+  return (
+    <Image
+      src={src}
+      alt="some image"
+      width="707"
+      height="471"
+      placeholder={"blur"}
+      priority
+      blurDataURL={base64}
+    />
+  );
 }
